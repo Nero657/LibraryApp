0f1b95ca0fc2b16ac2c0724cb9e53df42cda7ba4
Nero657 <nero657pl@gmail.com>
2023-03-19 16:27:06 +0100

    v2023.03.19_1

--- src/pl/javastart/library/app/LibraryControl.java
+++ src/pl/javastart/library/app/LibraryControl.java
@@ -5,67 +5,33 @@ import pl.javastart.library.model.Book;
 import pl.javastart.library.model.Library;
 import pl.javastart.library.model.Magazine;
 
-public class LibraryControl {
-    // zmienne do kontrolowania programu
-    private static final int EXIT = 0;
-    private static final int ADD_BOOK = 1;
-    private static final int ADD_MAGAZINE = 2;
-    private static final int PRINT_BOOKS = 3;
-    private static final int PRINT_MAGAZINES = 4;
-
-    // zmienna do komunikacji z użytkownikiem
+class LibraryControl {
     private DataReader dataReader = new DataReader();
 
-    // "biblioteka" przechowująca dane
     private Library library = new Library();
 
-    /*
-     * Główna metoda programu, która pozwala na wybór opcji i interakcję
-     */
-    public void controlLoop() {
-        int option;
+    void controlLoop() {
+        Option option;
 
         do {
             printOptions();
-            option = dataReader.getInt();
+            option = Option.createFromInt(dataReader.getInt());
             switch (option) {
-                case ADD_BOOK:
-                    addBook();
-                    break;
-                case ADD_MAGAZINE:
-                    addMagazine();
-                    break;
-                case PRINT_BOOKS:
-                    printBooks();
-                    break;
-                case PRINT_MAGAZINES:
-                    printMagazines();
-                    break;
-                case EXIT:
-                    exit();
-                    break;
-                default:
-                    System.out.println("Nie ma takiej opcji, wprowadź ponownie: ");
+                case ADD_BOOK -> addBook();
+                case ADD_MAGAZINE -> addMagazine();
+                case PRINT_BOOKS -> printBooks();
+                case PRINT_MAGAZINES -> printMagazines();
+                case EXIT -> exit();
+                default -> System.out.println("Nie ma takiej opcji, wprowadź ponownie: ");
             }
-        } while (option != EXIT);
-    }
-
-    private void printMagazines() {
-        library.printMagazines();
-    }
-
-    private void addMagazine() {
-        Magazine magazine = dataReader.readAndCreateMagazine();
-        library.addMagazine(magazine);
+        } while (option != Option.EXIT);
     }
 
     private void printOptions() {
         System.out.println("Wybierz opcję: ");
-        System.out.println(EXIT + " - wyjście z programu");
-        System.out.println(ADD_BOOK + " - dodanie nowej książki");
-        System.out.println(ADD_MAGAZINE + " - dodanie nowego magazynu");
-        System.out.println(PRINT_BOOKS + " - wyświetl dostępne książki");
-        System.out.println(PRINT_MAGAZINES + " - wyświetl dostępne magazyny");
+        for (Option option : Option.values()) {
+            System.out.println(option);
+        }
     }
 
     private void addBook() {
@@ -75,6 +41,15 @@ public class LibraryControl {
 
     private void printBooks() {
         library.printBooks();
+    }
+
+    private void addMagazine() {
+        Magazine magazine = dataReader.readAndCreateMagazine();
+        library.addMagazine(magazine);
+    }
+
+    private void printMagazines() {
+        library.printMagazines();
     }
 
     private void exit() {
